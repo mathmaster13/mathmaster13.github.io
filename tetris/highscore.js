1502b6f018de43b88a3ef1bc2c35fc8b1389799c
Sean <74142852+mathmaster13@users.noreply.github.com>
2026-04-20 07:25:30 -0400

    Update highscore.js

--- tetris/highscore.js
+++ tetris/highscore.js
@@ -20,50 +20,6 @@ String.prototype.chunked = function(size) {
     return this.match(new RegExp(`.{1,${size}}`, 'g'));
 };
 
-class HighScore {
-    constructor(name1 = "",
-                     score = 0,
-                     lines = 0,
-                     startLevel = 0,
-                     endLevel = 0) {
-        this.name = name1.padEnd(8, ' ');
-        this.score = score; this.lines = lines;
-        this.startLevel = startLevel; this.endLevel = endLevel;
-    };
-    
-    toUByteArray() {
-        let output = stringToBytes(this.name);
-        // score, in BCD
-        output += Uint8Array.fromHex(this.score.toString().padStart(8, '0'));
-        // lines, in BCD
-        output += Uint8Array.fromHex(this.lines.toString().padStart(4, '0'));
-
-        // start/end level, in hex
-        output += new Uint8Array([this.startLevel, this.endLevel]);
-        return output;
-    }
-}
-
-function isStringClean(input) {
-    for (char of CHARACTERS)
-        if (!CHARACTERS.includes(char)) 
-            return false;
-    return true;
-}
-
-function stringToBytes(input) {
-    const buffer = new ArrayBuffer(input.length);
-    const output = new Uint8Array(buffer);
-    for (let i = 0; i < input.length; i++) {
-        output[i] = CHARACTERS.indexOf(input[i]);
-    }
-    return output;
-}
-
-String.prototype.chunked = function(size) {
-    return this.match(new RegExp(`.{1,${size}}`, 'g'));
-};
-
 function concatenate(uint8arrays) {
   const totalLength = uint8arrays.reduce(
     (total, uint8array) => total + uint8array.byteLength,
